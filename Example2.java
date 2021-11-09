066a8fecdf2b68fababf7c56c29e6e888740c992
Abayomi AYOOLA <a.ayoola@gmail.com>
2021-11-09 16:47:04 -0700

    Add challenges codes

---
+++ Example2.java
@@ -0,0 +1,22 @@
+/*
+
+		This is a simple Java program.
+
+		Call this file Example2.java.
+
+*/
+
+class Example2 { 
+	public static void main(String args[]) {
+		int var1; //This declares a variable.
+		int var2; //this declares another varible
+
+		var1 = 1024; //this assigns 1024 to var1
+		System.out.println("var1 contains " + var1);
+
+		var2 = var1 / 2;
+
+		System.out.print("var2 contains var1 / 2: ");
+		System.out.println(var2);
+	}
+}
